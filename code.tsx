7a91ec6ee680c6825ac10f47cf150fa4fa9b2e42
KhangM <63178363+zasadg@users.noreply.github.com>
2025-05-28 18:37:04 +0700

    Add files via upload

---
+++ code.tsx
@@ -0,0 +1,91 @@
+"use client";
+import Image from 'next/image';
+
+export default function Hero() {
+    return (
+        <>
+            <section className="relative min-h-screen relative w-full lg:px-32 px-16 py-16 flex flex-col justify-center items-left" id="code">
+                <div className="">
+                    <div className="">
+                        <h1 className="text-4xl md:text-5xl font-bold mb-6 text-[#4B51BF]">Coding</h1>
+                        <p className="text-lg text-gray-700 pb-16">
+                           To keep in touch with my coding journey, those are some projects t   hat I have worked on.
+                        </p>
+
+                        {/* Card Section 1*/}
+                        <div className="relative">
+                            <div className="bg-[#4A5A84] rounded-lg flex flex-col md:flex-row items-stretch p-8 md:pr-[36rem] relative overflow-visible min-h-[260px]">
+                                {/* Text Section */}
+                                <div className="flex-1 flex flex-col justify-center text-white z-10 max-w-lg">
+                                    <h1 className="text-xl md:text-3xl font-bold pb-4">Alien invasion</h1>
+                                    <div className="text-xl md:text-xl pb-4">
+                                        A space shooting games for scoring points and compete with coworkers
+                                    </div>
+                                    <a href="#" className="underline text-white font-semibold text-xl hover:text-purple-200 transition-colors">See more →</a>
+                                </div>
+                                {/* Image Section */}
+                                <div className="hidden md:block">
+                                    <Image
+                                        src="/projects_images/Other_thumbnail_1.png"
+                                        alt="code screenshot"
+                                        width={520}
+                                        height={292}
+                                        className="absolute top-1/2 right-0 -translate-y-1/2 -translate-x-1/8 w-[520px] max-w-none drop-shadow-2xl rounded-lg z-20"
+                                        loading="lazy"
+                                    />
+                                </div>
+                                {/* Mobile Image */}
+                                <div className="block md:hidden mt-8">
+                                    <Image
+                                        src="/projects_images/Other_thumbnail_1.png"
+                                        alt="code screenshot"
+                                        width={520}
+                                        height={292}
+                                        className="w-full rounded-lg"
+                                        loading="lazy"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Card Section 2*/}
+                        <div className="relative pt-16">
+                            <div className="bg-[#4B51BF] rounded-lg flex flex-col md:flex-row items-stretch p-8 md:pr-[36rem] relative overflow-visible min-h-[260px]">
+                                {/* Text Section */}
+                                <div className="flex-1 flex flex-col justify-center text-white z-10 max-w-lg">
+                                    <h1 className="text-xl md:text-3xl font-bold pb-4">Interactive whiteboard</h1>
+                                    <div className="text-xl md:text-xl pb-4">
+                                        My personal experience with interact.js and gpt
+                                    </div>
+                                    <a href="https://my-portfolio-2l3f.vercel.app" target="_blank" rel="noopener noreferrer" className="underline text-white font-semibold text-xl hover:text-purple-200 transition-colors">See more →</a>
+                                </div>
+                                {/* Image Section */}
+                                <div className="hidden md:block">
+                                    <Image
+                                        src="/projects_images/Other_3.png"
+                                        alt="code screenshot"
+                                        width={520}
+                                        height={292}
+                                        className="absolute top-1/2 right-0 -translate-y-1/2 -translate-x-1/8 w-[520px] max-w-none drop-shadow-2xl rounded-xl z-20"
+                                        loading="lazy"
+                                    />
+                                </div>
+                                {/* Mobile Image */}
+                                <div className="block md:hidden mt-8">
+                                    <Image
+                                        src="/projects_images/Other_3.png"
+                                        alt="code screenshot"
+                                        width={520}
+                                        height={292}
+                                        className="w-full rounded-xl"
+                                        loading="lazy"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
